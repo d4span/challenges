eda75cb6bbff4b16c0cb59ac834fdef81934c11d
David Caspar <david.caspar@aol.ch>
2015-05-31 23:23:29 +0200

    Next problem: https://www.hackerrank.com/challenges/angry-professor

--- src/warmup/angry_children.clj
+++ src/warmup/angry_children.clj
@@ -1,3 +1,23 @@
 (ns warmup.angry-children)
 
 ;; https://www.hackerrank.com/challenges/angry-children
+
+(def N (Long/parseLong (read-line)))
+
+(def K (Long/parseLong (read-line)))
+
+(def numbers
+  (loop [n 0
+         nums []]
+    (if (< n N)
+      (recur (inc n) (conj nums (Long/parseLong (read-line))))
+      (vec (sort nums)))))
+
+(loop [n    0
+       ufs  []]
+  (if (< (- N K n) 1)
+    (println (reduce min ufs))
+    (let [ufmin (numbers n)
+          ufmax (numbers (- (+ n K) 1))
+          uf (- ufmax ufmin)]
+      (recur (inc n) (conj ufs uf)))))
---
+++ src/warmup/angry_professor.clj
@@ -0,0 +1,1 @@
+(ns warmup.angry-professor)
